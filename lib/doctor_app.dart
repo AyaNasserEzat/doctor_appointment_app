f50380950b10b221b12994d12f1bcb9582f11947
unknown <ayanasser1853@gmail.com>
2025-10-21 23:52:17 +0300

    chore: initialize Flutter project

---
+++ lib/doctor_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DoctorApp extends StatelessWidget {
+  const DoctorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:doctor_appointment/doctor_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DoctorApp());
+}
