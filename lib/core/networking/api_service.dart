7ebaaa381b1ece7e35319daa7a5d09515935423c
unknown <ayanasser1853@gmail.com>
2025-10-23 04:30:03 +0300

    feat: add login endpoint to ApiService using POST request

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:doctor_appointment/core/networking/api_constant.dart';
+import 'package:doctor_appointment/feature/login/data/models/login_request_body.dart';
+import 'package:doctor_appointment/feature/login/data/models/login_response.dart';
 import 'package:retrofit/error_logger.dart';
 import 'package:retrofit/http.dart';
 part 'api_service.g.dart';
@@ -7,4 +9,7 @@ part 'api_service.g.dart';
 @RestApi(baseUrl: ApiConstants.apiBaseUrl)
 abstract class ApiService {
   factory ApiService(Dio dio, {String? baseUrl}) = _ApiService;
+
+  @POST(ApiConstants.login)
+  Future<LoginResponse> login(@Body() LoginRequestBody loginRequestBody);
 }
