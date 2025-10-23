40c6038788fc4c8a6c93ef10a42a8aa40636717f
unknown <ayanasser1853@gmail.com>
2025-10-23 05:43:57 +0300

    chore: setup dependency injection using GetIt

---
+++ lib/core/di/depensency_injection.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:doctor_appointment/core/networking/api_service.dart';
+import 'package:doctor_appointment/core/networking/dio_factory.dart';
+import 'package:doctor_appointment/feature/login/data/repos/login_repo.dart';
+import 'package:doctor_appointment/feature/login/prsentation/logic/cubit/login_cubit.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+void setUpGetIt() {
+  Dio dio = DioFactory.getDio();
+  getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
+  //login
+  getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt()));
+  getIt.registerLazySingleton<LoginCubit>(() => LoginCubit(getIt()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:doctor_appointment/core/di/depensency_injection.dart';
 import 'package:doctor_appointment/doctor_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  setUpGetIt();
   runApp(const DoctorApp());
 }
