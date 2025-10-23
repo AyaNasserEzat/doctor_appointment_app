96e3d9decf200aa4041e20e5c619fc3f373ab843
unknown <ayanasser1853@gmail.com>
2025-10-23 14:30:53 +0300

    fix(api): change errors field to dynamic to support List and Map

--- lib/core/networking/api_error_model.dart
+++ lib/core/networking/api_error_model.dart
@@ -6,25 +6,14 @@ class ApiErrorModel {
   final String? message;
   final int? code;
   @JsonKey(name: 'data')
-  final Map<String, dynamic>? errors;
+  final dynamic errors;
 
   ApiErrorModel({this.message, this.code, this.errors});
 
   factory ApiErrorModel.fromJson(Map<String, dynamic> json) =>
       _$ApiErrorModelFromJson(json);
   Map<String, dynamic> toJson() => _$ApiErrorModelToJson(this);
-  // factory ApiErrorModel.fromJson(Map<String, dynamic> json) {
-  //   return ApiErrorModel(
-  //     message: json['message'],
-  //     code: json['code'],
-  //     errors:
-  //         json['data'] != null && json['data'] is Map<String, dynamic>
-  //             ? (json['data'] as Map<String, dynamic>).map(
-  //               (key, value) => MapEntry(key, List<String>.from(value)),
-  //             )
-  //             : null,
-  //   );
-  // }
+
   String getAllErrorMessages() {
     if (errors == null || errors!.isEmpty) {
       return message ?? "Unknown error occurred";
