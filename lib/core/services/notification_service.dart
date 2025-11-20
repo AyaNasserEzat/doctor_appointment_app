aeab758b9c0cd22409ead2a4d231539cb4ae1e69
unknown <ayanasser1853@gmail.com>
2025-11-20 02:19:53 +0200

    Use flutter_timezone package and set tz.local based on device timezone not use (static Africa/Cairo)

--- lib/core/services/notification_service.dart
+++ lib/core/services/notification_service.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:flutter_timezone/flutter_timezone.dart';
 
 class NotificationService {
   static FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
@@ -28,12 +29,15 @@ class NotificationService {
   //
   static showScheduleNotification() async {
     tz.initializeTimeZones();
+    final TimezoneInfo currentTimeZone =
+        await FlutterTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(currentTimeZone.identifier));
     await flutterLocalNotificationsPlugin.zonedSchedule(
       1,
       "title",
       "body",
       // tz.TZDateTime.now(tz.local).add(Duration(seconds: 10)),
-      tz.TZDateTime(tz.getLocation('Africa/Cairo'), 2025, 11, 20, 1, 57),
+      tz.TZDateTime(tz.local, 2025, 11, 20, 2, 18),
       NotificationDetails(
         android: AndroidNotificationDetails(
           'id1',
