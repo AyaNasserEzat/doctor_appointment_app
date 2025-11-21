4fc53d8d62bb639cbe92c8eabe0aee53c16a45d8
unknown <ayanasser1853@gmail.com>
2025-11-21 02:43:12 +0200

    Move token initialization logic from DioFactory to Interceptor- Removed Authorization header from DioFactory to avoid reading empty token on first app launch. - Kept token loading inside ApiInterceptors only. - Fixed issue where the token was null on the first run because DioFactory was assigning headers before SecureStorage finished saving the token. - Now headers are always updated dynamically through the interceptor on every request. - This ensures the token is attached correctly even on the first app start.

--- lib/core/networking/api_interceptore.dart
+++ lib/core/networking/api_interceptore.dart
@@ -5,10 +5,15 @@ import 'package:doctor_appointment/core/helper/shared_pref_helper.dart';
 class ApiInterceptors extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) async{
-    options.headers[SharedPrefKeys.userToken] =
+    options.headers['Authorization'] =
        await SharedPrefHelper.getSecuredString(SharedPrefKeys.userToken) != null
             ? 'Bearer ${await SharedPrefHelper.getSecuredString(SharedPrefKeys.userToken)}'
             : null;
+    //    final token = await SharedPrefHelper.getSecuredString(SharedPrefKeys.userToken);
+
+    // if (token != null && token.isNotEmpty) {
+    //   options.headers['Authorization'] = 'Bearer $token';
+    // }
 
     super.onRequest(options, handler);
   }
